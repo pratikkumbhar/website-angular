d1d9c49bbe18ddb323120707ddd6858c9a4102b6
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-21 08:58:09 +0530

    tailwind color preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-     "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      primary: "#0E3998",
+      secondary: "#F3F1F6",
+      accent: "#FFBC01",
+      myblack: "#150E1F",
+      mygray: "#554B4E",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
