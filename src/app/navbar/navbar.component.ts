2385589508d2fe87c4c580fb7f8e1393230e5713
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-23 17:42:42 +0530

    about done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  navClicked:boolean = true
+  navIconChange(){
+    if(this.navClicked){
 
+      this.navClicked = false
+    }
+    else{
+      this.navClicked = true
+    }
+  }
 }
