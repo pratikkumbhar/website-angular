9f60c4b318e5932569f7057bd62fdf518461a854
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-23 08:44:19 +0530

    responsive issue solve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { url } from 'node:inspector';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
