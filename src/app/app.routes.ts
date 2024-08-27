fd99e99c5e7b9f2607a9492aa7760ab17e7cbf24
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-27 07:59:27 +0530

    no

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,22 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  navClicked:boolean = true
-  navIconChange(){
-    if(this.navClicked){
-
-      this.navClicked = false
-    }
-    else{
-      this.navClicked = true
+  navClicked: boolean = true;
+  navIconChange() {
+    if (this.navClicked) {
+      this.navClicked = false;
+    } else {
+      this.navClicked = true;
     }
   }
 }
