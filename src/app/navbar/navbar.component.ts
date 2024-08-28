937eb696c53405297de66fe86da48a00f2dcb4ce
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-28 17:46:35 +0530

    28 aug 5.46

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule,RouterLink,RouterOutlet,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+
+  isActive:any
+  
   navClicked: boolean = true;
+  constructor(){}
+  
+
   navIconChange() {
     if (this.navClicked) {
       this.navClicked = false;
