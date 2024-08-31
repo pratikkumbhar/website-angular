6adc71619ed0e4f346c061440392f47133b7de22
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-08-31 09:02:03 +0530

    almost done

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-services',
   standalone: true,
   imports: [],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
+  success: boolean = false;
 
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_5y5dcc1',
+        'template_ojmg5qo',
+        e.target as HTMLFormElement,
+        {
+          publicKey: 'dZL6O7XIqGpv0TDEo',
+        }
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Form is Submited Successfully!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+          alert('System Error');
+        }
+      );
+  }
 }
