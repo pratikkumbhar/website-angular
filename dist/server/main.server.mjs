dba19457a8119f2ecf647119a62eaa8d6f5b0f28
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-01 22:34:55 +0530

    build and netlify

---
+++ dist/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3LWFS37U.mjs";import"./chunk-YFRGEN2Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  success: boolean = false;
 
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_5y5dcc1',
+        'template_ojmg5qo',
+        e.target as HTMLFormElement,
+        {
+          publicKey: 'dZL6O7XIqGpv0TDEo',
+        }
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Form is Submited Successfully!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+          alert('System Error');
+        }
+      );
+  }
 }
