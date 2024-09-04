befdd073c68ca79e4890eca05c59fe828f1988f6
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-04 09:58:17 +0530

    04 sep

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { FooterComponent } from "../footer/footer.component";
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
